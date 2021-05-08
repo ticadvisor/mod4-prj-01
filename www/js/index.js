69de4851391e64622b6083a0b5c6e1018621d75e
ticadvisor <javier.herrero@ticadvisor.com>
2021-05-08 10:44:41 +0200

    Initial Version v.0.0

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,20 @@
-'use strict';
-
-
+/**
+ * #################
+ * ## 👻 B O O 👻 ##
+ * #################
+ *
+ * Haz que, cada segundo que pase, el div "boo" aparezca ubicado en una
+ * posición aleatoria de la ventana y que el color del body cambie, también
+ * de forma aleatoria "rgb(?, ?, ?)".
+ *
+ * ¡Ojo! No dejes que Boo se salga de los márgenes de la ventana, debemos
+ * poder ver a Boo sin hacer scroll en ningún momento.
+ *
+ * element.getBoundingClientRect() --> devuelve una serie de propiedades
+ * del nodo seleccionado, entre ellas, la altura y el ancho del elemento.
+ *
+ * Bonus point --> haz que aparezcan más emojis acompañando a Boo. Para ello
+ *                 deberás crear más divs y seleccionarlos a todos. Esto
+ *                 requerirá a mayores que cambies cositas en el código.
+ *
+ */
